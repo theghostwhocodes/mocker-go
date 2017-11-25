21a511b0f9e9350573bf99743c64b25ba931ee20
Paolo Ferretti <paolo.ferretti@gmail.com>
2017-11-25 18:26:11 +0100

    Better command line args management

--- cmd/mocker/main.go
+++ cmd/mocker/main.go
@@ -46,19 +46,38 @@ func handlerFactory(basePath string) func(w http.ResponseWriter, r *http.Request
 	}
 }
 
+var dataPath string
+var port int
+var host string
+
+func init() {
+	const (
+		dataPathDefault  = "./data"
+		dataPathHelpText = "The data path"
+		portDefault      = 8000
+		portHelpText     = "The TCP port to listen"
+		hostDefault      = "127.0.0.1"
+		hostHelpText     = "The host to listen"
+	)
+
+	flag.StringVar(&dataPath, "d", dataPathDefault, dataPathHelpText+" (shorthand)")
+	flag.StringVar(&dataPath, "data", dataPathDefault, dataPathHelpText)
+	flag.IntVar(&port, "p", portDefault, portHelpText+" (shorthand)")
+	flag.IntVar(&port, "port", portDefault, portHelpText)
+	flag.StringVar(&host, "h", hostDefault, hostHelpText+" (shorthand)")
+	flag.StringVar(&host, "host", hostDefault, hostHelpText)
+}
+
 func main() {
-	dataPath := flag.String("path", "./data", "The data path")
-	port := flag.Int("port", 8000, "The TCP port to listen")
-	host := flag.String("host", "127.0.0.1", "The host to listen")
 	flag.Parse()
 
-	basePath, _ := filepath.Abs(*dataPath)
+	basePath, _ := filepath.Abs(dataPath)
 
 	http.HandleFunc("/", handlerFactory(basePath))
 
 	log.Printf("Loading data from %s", basePath)
-	log.Printf("Mocker listening on %s:%d...", *host, *port)
+	log.Printf("Mocker listening on %s:%d...", host, port)
 
-	address := fmt.Sprintf("%s:%d", *host, *port)
+	address := fmt.Sprintf("%s:%d", host, port)
 	http.ListenAndServe(address, nil)
 }
