8eeb1bab248581bec9932929ad84062ce91cabf8
Paolo Ferretti <paolo.ferretti@gmail.com>
2017-12-02 16:46:27 +0100

    Tests

--- internal/contentManagers/contentManagers.go
+++ internal/contentManagers/contentManagers.go
@@ -8,14 +8,22 @@ import (
 	"strings"
 )
 
-func GetContent(basePath string, r *http.Request) ([]byte, error) {
-	method := r.Method
-	fileName := fmt.Sprintf("%s.%s.json", r.URL.Path[1:], strings.ToUpper(method))
-	content, err := ioutil.ReadFile(
-		path.Join(
-			basePath,
-			fileName,
-		),
+// GetFileName return the computed file name for the mock
+func GetFileName(urlPath string, method string) string {
+	return fmt.Sprintf("%s.%s.json", urlPath, strings.ToUpper(method))
+}
+
+// GetAbsoluteFileName return the full mock filename
+func GetAbsoluteFileName(basePath string, fileName string) string {
+	return path.Join(
+		basePath,
+		fileName,
 	)
+}
+
+// GetContent return the mock content
+func GetContent(basePath string, r *http.Request) ([]byte, error) {
+	fileName := GetFileName(r.URL.Path[1:], r.Method)
+	content, err := ioutil.ReadFile(GetAbsoluteFileName(basePath, fileName))
 	return content, err
 }
