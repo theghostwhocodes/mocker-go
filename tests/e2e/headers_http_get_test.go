744ea6e2414d95e2abf9c9bb971cb58ef3dfc5f9
Paolo Ferretti <paolo.ferretti@fastmail.com>
2018-04-15 23:00:38 +0200

    TestSimpleHttpGetSingleHeader

---
+++ tests/e2e/headers_http_get_test.go
@@ -0,0 +1,42 @@
+package integrationtests
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"testing"
+)
+
+func TestSimpleHttpGetSingleHeader(t *testing.T) {
+	url := fmt.Sprintf("%s/simple", ts.URL)
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		t.Fail()
+	}
+	req.Header.Add("X-Custom-Header", "Custom header")
+	res, err := client.Do(req)
+
+	if res.StatusCode != 200 {
+		t.Fail()
+	}
+
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Fail()
+	}
+
+	var content interface{}
+	err = json.Unmarshal(body, &content)
+	if err != nil {
+		t.Fail()
+	}
+
+	value := content.(map[string]interface{})
+	if value["key"] != "simple.GET.singleheader.json" {
+		t.Fail()
+	}
+}
