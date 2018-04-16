27f9fd2b096272269aeaa8e9e3a9dc8f15fe265a
Paolo Ferretti <paolo.ferretti@fastmail.com>
2018-04-16 19:12:33 +0200

    TestSimpleHttpGetDoubleHeader

--- tests/e2e/headers_http_get_test.go
+++ tests/e2e/headers_http_get_test.go
@@ -40,3 +40,37 @@ func TestSimpleHttpGetSingleHeader(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func TestSimpleHttpGetDoubleHeader(t *testing.T) {
+	url := fmt.Sprintf("%s/simple", ts.URL)
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		t.Fail()
+	}
+	req.Header.Add("X-Custom-Header", "Custom header")
+	req.Header.Add("X-Custom-Header-2", "Custom header 2")
+	res, err := client.Do(req)
+
+	if res.StatusCode != 200 {
+		t.Fail()
+	}
+
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Fail()
+	}
+
+	var content interface{}
+	err = json.Unmarshal(body, &content)
+	if err != nil {
+		t.Fail()
+	}
+
+	value := content.(map[string]interface{})
+	if value["key"] != "simple.GET.doubleheader.json" {
+		t.Fail()
+	}
+}
