bf6116d99e101e850a9c346c9bf17bcdb40a8d1e
Paolo Ferretti <paolo.ferretti@fastmail.com>
2018-05-02 23:01:58 +0200

    TestSimpleHttpPutTwoParams

--- tests/e2e/simple_http_put_test.go
+++ tests/e2e/simple_http_put_test.go
@@ -130,3 +130,48 @@ func TestSimpleHttpPutOneParam(t *testing.T) {
 		t.Fail()
 	}
 }
+
+// TestSimpleHttpPutTwoParams tests a simple HTTP PUT call using a stub file with HTTP
+// verb explicitly set in filename and two parameters in stub content
+func TestSimpleHttpPutTwoParams(t *testing.T) {
+	urlString := fmt.Sprintf("%s/simple", ts.URL)
+	form := url.Values{
+		"param1": {"value1"},
+		"param2": {"value2"},
+	}
+	postBody := bytes.NewBufferString(form.Encode())
+	client := &http.Client{}
+	req, err := http.NewRequest("PUT", urlString, postBody)
+	if err != nil {
+		t.Fail()
+	}
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	res, err := client.Do(req)
+
+	if err != nil {
+		t.Fail()
+	}
+
+	if res.StatusCode != 200 {
+		t.Fail()
+	}
+
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Fail()
+	}
+
+	var content interface{}
+	err = json.Unmarshal(body, &content)
+	if err != nil {
+		t.Fail()
+	}
+
+	value := content.(map[string]interface{})
+	fmt.Printf("%v", value)
+	if value["key"] != "simple.PUT.twoparams.json" {
+		t.Fail()
+	}
+}
