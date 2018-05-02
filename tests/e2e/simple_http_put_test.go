d5b2d11c1ef2df27ae7b89391f33fe84d29280ea
Paolo Ferretti <paolo.ferretti@fastmail.com>
2018-05-02 22:46:20 +0200

    TestSimpleHttpPutNoVerbInFile

--- tests/e2e/simple_http_put_test.go
+++ tests/e2e/simple_http_put_test.go
@@ -46,3 +46,41 @@ func TestSimpleHttpPut(t *testing.T) {
 		t.Fail()
 	}
 }
+
+// TestSimpleHttpPutNoVerbInFile tests a simple HTTP PUT call using a stub file with HTTP
+// verb NOT explicitly set in filename
+func TestSimpleHttpPutNoVerbInFile(t *testing.T) {
+	url := fmt.Sprintf("%s/simplePUT", ts.URL)
+	client := &http.Client{}
+	req, err := http.NewRequest("PUT", url, nil)
+	if err != nil {
+		t.Fail()
+	}
+	req.Header.Add("Custom-Type", "application/form-data")
+	res, err := client.Do(req)
+	if err != nil {
+		t.Fail()
+	}
+
+	if res.StatusCode != 200 {
+		t.Fail()
+	}
+
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Fail()
+	}
+
+	var content interface{}
+	err = json.Unmarshal(body, &content)
+	if err != nil {
+		t.Fail()
+	}
+
+	value := content.(map[string]interface{})
+	if value["key"] != "simplePUT.HTTP.json" {
+		t.Fail()
+	}
+}
