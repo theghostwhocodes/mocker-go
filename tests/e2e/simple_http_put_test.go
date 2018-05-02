c9e40271ad8ed02ff3669bfa54f7174ade9ccf65
Paolo Ferretti <paolo.ferretti@fastmail.com>
2018-05-02 22:58:59 +0200

    TestSimpleHttpPutOneParam

--- tests/e2e/simple_http_put_test.go
+++ tests/e2e/simple_http_put_test.go
@@ -1,10 +1,12 @@
 package integrationtests
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"testing"
 )
 
@@ -17,7 +19,7 @@ func TestSimpleHttpPut(t *testing.T) {
 	if err != nil {
 		t.Fail()
 	}
-	req.Header.Add("Custom-Type", "application/form-data")
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 	res, err := client.Do(req)
 
 	if err != nil {
@@ -56,7 +58,7 @@ func TestSimpleHttpPutNoVerbInFile(t *testing.T) {
 	if err != nil {
 		t.Fail()
 	}
-	req.Header.Add("Custom-Type", "application/form-data")
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 	res, err := client.Do(req)
 	if err != nil {
 		t.Fail()
@@ -81,6 +83,50 @@ func TestSimpleHttpPutNoVerbInFile(t *testing.T) {
 
 	value := content.(map[string]interface{})
 	if value["key"] != "simplePUT.HTTP.json" {
+		t.Fail()
+	}
+}
+
+// TestSimpleHttpPutOneParam tests a simple HTTP PUT call using a stub file with HTTP
+// verb explicitly set in filename and one parameter in stub content
+func TestSimpleHttpPutOneParam(t *testing.T) {
+	urlString := fmt.Sprintf("%s/simple", ts.URL)
+	form := url.Values{
+		"param1": {"value1"},
+	}
+	postBody := bytes.NewBufferString(form.Encode())
+	client := &http.Client{}
+	req, err := http.NewRequest("PUT", urlString, postBody)
+	if err != nil {
+		t.Fail()
+	}
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	res, err := client.Do(req)
+
+	if err != nil {
+		t.Fail()
+	}
+
+	if res.StatusCode != 200 {
+		t.Fail()
+	}
+
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Fail()
+	}
+
+	var content interface{}
+	err = json.Unmarshal(body, &content)
+	if err != nil {
+		t.Fail()
+	}
+
+	value := content.(map[string]interface{})
+	fmt.Printf("%v", value)
+	if value["key"] != "simple.PUT.param.json" {
 		t.Fail()
 	}
 }
