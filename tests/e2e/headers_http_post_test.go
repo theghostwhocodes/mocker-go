54cabc19d5fd7bfe5c2203d61dd46936d2b271ea
Paolo Ferretti <paolo.ferretti@fastmail.com>
2018-05-02 22:25:58 +0200

    TestSimpleHttpPostHeaderNoMatch

--- tests/e2e/headers_http_post_test.go
+++ tests/e2e/headers_http_post_test.go
@@ -74,3 +74,36 @@ func TestSimpleHttpPostDoubleHeader(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func TestSimpleHttpPostHeaderNoMatch(t *testing.T) {
+	url := fmt.Sprintf("%s/simple", ts.URL)
+	client := &http.Client{}
+	req, err := http.NewRequest("POST", url, nil)
+	if err != nil {
+		t.Fail()
+	}
+	req.Header.Add("X-Custom-Headerr", "Custom header")
+	res, err := client.Do(req)
+
+	if res.StatusCode != 200 {
+		t.Fail()
+	}
+
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Fail()
+	}
+
+	var content interface{}
+	err = json.Unmarshal(body, &content)
+	if err != nil {
+		t.Fail()
+	}
+
+	value := content.(map[string]interface{})
+	if value["key"] != "simple.POST.json" {
+		t.Fail()
+	}
+}
