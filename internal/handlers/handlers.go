f76c69c2f1c865a34181ec50777637211bfa0b1d
Paolo Ferretti <paolo.ferretti@fastmail.com>
2018-05-05 22:43:30 +0200

    A simple working version for proxy

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -30,20 +31,62 @@ func manageSuccess(w http.ResponseWriter, r *http.Request, content []byte) {
 // HandlerFactory return a proper handler
 func HandlerFactory(basePath string, proxyFor string) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-		jsonMaps, err := contentmanagers.GetScannedMockContent(basePath, r)
-		if err != nil {
-			sendErrorMessage(w, err.Error())
-			return
+		res := new(http.Response)
+		var body []byte
+		var err error
+
+		if proxyFor != "" {
+			fmt.Printf("ProxyFor\n")
+			urlPath := r.URL.Path[1:]
+			url := fmt.Sprintf("%s/%s", proxyFor, urlPath)
+			httpVerb := r.Method
+			httpHeaders := r.Header
+			httpBody := r.Body
+			fmt.Printf("Calling %s using verb %s...\n", url, httpVerb)
+			// fmt.Printf("Headers %v\n", httpHeaders)
+			// fmt.Printf("Body %v", httpBody)
+			client := &http.Client{}
+			req, err := http.NewRequest(httpVerb, url, httpBody)
+			if err != nil {
+				sendErrorMessage(w, err.Error())
+				return
+			}
+			for key, value := range httpHeaders {
+				req.Header.Add(key, value[0])
+			}
+			res, err = client.Do(req)
+			fmt.Printf("A Response status code %d\n", res.StatusCode)
+			if err != nil {
+				sendErrorMessage(w, err.Error())
+				return
+			}
+
+			body, err = ioutil.ReadAll(res.Body)
+			if err != nil {
+				sendErrorMessage(w, err.Error())
+				return
+			}
 		}
 
-		if len(jsonMaps) < 1 {
-			w.WriteHeader(http.StatusNotFound)
-			return
+		fmt.Printf("Response status code %d\n", res.StatusCode)
+		if res.StatusCode == 404 || res.StatusCode == 405 || err != nil || proxyFor == "" {
+			fmt.Printf("Normal\n")
+			jsonMaps, err := contentmanagers.GetScannedMockContent(basePath, r)
+			if err != nil {
+				sendErrorMessage(w, err.Error())
+				return
+			}
+
+			if len(jsonMaps) < 1 {
+				w.WriteHeader(http.StatusNotFound)
+				return
+			}
+
+			jsonMap := jsonMaps[0]
+
+			body, _ = json.Marshal(jsonMap.Response.Body)
 		}
 
-		jsonMap := jsonMaps[0]
-
-		body, _ := json.Marshal(jsonMap.Response.Body)
 		manageSuccess(w, r, body)
 	}
 }
