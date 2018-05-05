902c1ad4f54bf10f76b8825027f70457c4d9b368
Paolo Ferretti <paolo.ferretti@fastmail.com>
2018-05-05 10:17:29 +0200

    TestSimpleHttpPutDoubleHeader

--- tests/e2e/headers_http_put_test.go
+++ tests/e2e/headers_http_put_test.go
@@ -74,3 +74,36 @@ func TestSimpleHttpPutDoubleHeader(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func TestSimpleHttpPutHeaderNoMatch(t *testing.T) {
+	url := fmt.Sprintf("%s/simple", ts.URL)
+	client := &http.Client{}
+	req, err := http.NewRequest("PUT", url, nil)
+	if err != nil {
+		t.Fail()
+	}
+	req.Header.Add("X-Custom-Headerr", "Custom header")
+	res, err := client.Do(req)
+
+	if res.StatusCode != 200 {
+		t.Fail()
+	}
+
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Fail()
+	}
+
+	var content interface{}
+	err = json.Unmarshal(body, &content)
+	if err != nil {
+		t.Fail()
+	}
+
+	value := content.(map[string]interface{})
+	if value["key"] != "simple.PUT.json" {
+		t.Fail()
+	}
+}
