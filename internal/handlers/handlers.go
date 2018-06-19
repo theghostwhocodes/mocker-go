ffc2175ca56daf901750635ba6d476373fc7ae51
Paolo Ferretti <ferretti@mailupgroup.com>
2018-06-19 19:35:42 +0200

    Handling correctly response headers

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -22,8 +22,9 @@ func sendErrorMessage(w http.ResponseWriter, message string) {
 	fmt.Fprintln(w, errorMessage)
 }
 
-func manageSuccess(w http.ResponseWriter, r *http.Request, content []byte) {
+func manageSuccess(w http.ResponseWriter, r *http.Request, content []byte, status int) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(status)
 	fmt.Fprintln(w, string(content))
 	log.Printf("Serving %s\n", r.URL.Path[1:])
 }
@@ -72,7 +73,6 @@ func HandlerFactory(basePath string, proxyFor string) func(w http.ResponseWriter
 		}
 
 		w.Header().Set("Mocker-Stubbed", "true")
-		w.WriteHeader(httpStatusCode)
-		manageSuccess(w, r, body)
+		manageSuccess(w, r, body, httpStatusCode)
 	}
 }
